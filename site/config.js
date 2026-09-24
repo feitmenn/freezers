77ec489cc2e2d98ac39ecc1e87aeba2b276c662b
feitmennclub <jornelliseu@gmail.com>
2026-09-24 21:11:28 +0200

    Connect deployed site to Freezers ESPORT API

--- site/config.js
+++ site/config.js
@@ -1,2 +1,2 @@
 /* Public endpoint URL only; no GitHub secret or repository details belong here. */
-window.FREEZERS_API_URL = '';
+window.FREEZERS_API_URL = 'https://freezers-esport-api.jornelliseu-c7d.workers.dev';
